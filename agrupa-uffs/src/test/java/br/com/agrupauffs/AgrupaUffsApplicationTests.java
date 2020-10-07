185c28f851fb24ef697470227c6196bb40386f03
Rodrigo Rech <rodrigorech@hotmail.com>
2020-10-07 18:55:47 -0300

    Projeto base

---
+++ agrupa-uffs/src/test/java/br/com/agrupauffs/AgrupaUffsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.agrupauffs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgrupaUffsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
