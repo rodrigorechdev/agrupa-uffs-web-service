fba3f96ff0d3ec9b156b95ae2a0651b3d8662c6a
Rodrigo Rech <rodrigorech@hotmail.com>
2020-11-12 20:38:41 -0300

    endpoint login temporario

---
+++ agrupa-uffs/src/main/java/br/com/agrupauffs/controller/UsuariosController.java
@@ -0,0 +1,22 @@
+package br.com.agrupauffs.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.agrupauffs.DTO.LoginDTO;
+import br.com.agrupauffs.business.LoginBusiness;
+
+@RequestMapping("usuarios/")
+@RestController
+public class UsuariosController {
+
+	@RequestMapping(value = "login", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public boolean validarLogin(@RequestBody LoginDTO login){
+		var loginBusiness = new LoginBusiness();
+		return loginBusiness.validarLogin(login.getEmail(), login.getSenha());
+	}
+}
+
