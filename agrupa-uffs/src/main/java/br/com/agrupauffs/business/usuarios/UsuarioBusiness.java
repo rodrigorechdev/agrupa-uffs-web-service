d8da128901f3eb555db392ec2463ffc3165e8210
Rodrigo Rech <rodrigorech@hotmail.com>
2021-01-23 11:17:15 -0300

    endpoint consulta notificacoes
    Novo endpoint que retorna todas as notificações de um usuário

--- agrupa-uffs/src/main/java/br/com/agrupauffs/business/usuarios/UsuarioBusiness.java
+++ agrupa-uffs/src/main/java/br/com/agrupauffs/business/usuarios/UsuarioBusiness.java
@@ -1,9 +1,13 @@
 package br.com.agrupauffs.business.usuarios;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.agrupauffs.controller.parametros.NotificacoesDeUsuario;
 import br.com.agrupauffs.controller.parametros.VisualizaNotificacao;
+import br.com.agrupauffs.usuario.EntidadeNotificacao;
 import br.com.agrupauffs.usuario.QueryNotificacao;
 
 @Service
@@ -18,6 +22,15 @@ public class UsuarioBusiness {
      */
     public void visualizaNotificacao(VisualizaNotificacao body) {
         queryNotificacao.visualizaNotificacao(body.getIdNotificacao());
+    }
+
+    /**
+     * Retorna todas as notificações de um usuário
+     * @param body
+     * @return
+     */
+    public List<EntidadeNotificacao> notificacoesDeUsuario(NotificacoesDeUsuario body) {
+        return queryNotificacao.notificacoesDeUsuario(body.getIdUsuario());
     }
 
 }
